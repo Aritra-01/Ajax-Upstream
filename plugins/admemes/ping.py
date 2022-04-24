b6df98028ae913062131a3f567cfa5d92f37b4ed
Aritra <88967619+Aritra-01@users.noreply.github.com>
2022-04-25 00:44:57 +0530

    Update ping.py

--- plugins/admemes/ping.py
+++ plugins/admemes/ping.py
@@ -8,10 +8,10 @@ from info import COMMAND_HAND_LER
 from plugins.helper_functions.cust_p_filters import f_onw_fliter
 
 # -- Constants -- #
-ALIVE = "ചത്തിട്ടില്ല മുത്തേ ഇവിടെ തന്നെ ഉണ്ട്.. നിനക്ക് ഇപ്പൊ എന്നോട് ഒരു സ്നേഹവും ഇല്ല. കൊള്ളാം.. നീ പാഴെ പോലെയേ അല്ല മാറിപോയി..😔 ഇടക്ക് എങ്കിലും ചുമ്മാ ഒന്ന് /start ചെയ്തു നോക്ക്..🙂" 
-REPO = "<b>𝙳𝙴𝙿𝙻𝙾𝚈 𝚃𝚄𝚃𝙾𝚁𝙸𝙰𝙻 ›› https://youtu.be/kB9TkCs8cX0</b>"
-CHANNEL = "<b>𝚈𝙾𝚄𝚃𝚄𝙱𝙴 𝙲𝙷𝙰𝙽𝙽𝙴𝙻</b> ›› https://youtube.com/channel/UCf_dVNrilcT0V2R--HbYpMA\n\n<b>𝚄𝙿𝙳𝙰𝚃𝙴𝚂 𝙲𝙷𝙰𝙽𝙽𝙴𝙻 ›› https://t.me/OpusTechz</b>\n\n<b>𝙲𝙷𝙰𝙽𝙽𝙴𝙻 ›› https://t.me/MWUpdatez</b>"
-AJAX = "<b>𝙱𝙾𝚃 ›› https://t.me/Devil0Bot_Bot</b>"
+ALIVE = "You are not dead. You are still here. You have no love for me now. Well, you're not changed..😔 Sometimes but nothing /start Do it and see..🙂" 
+UNMESH = "<b>উন্মেষ Channel LINK ›› https://t.me/HoiChoiTvAddaa</b>"
+CHANNEL = "<b>জলতরঙ্গ 𝙲𝙷𝙰𝙽𝙽𝙴𝙻</b> ›› https://t.me/Joltorongo\n\n<b>𝚄𝙿𝙳𝙰𝚃𝙴𝚂 𝙲𝙷𝙰𝙽𝙽𝙴𝙻 ›› https://t.me/HoiChoiTvAddaa</b>\n\n<b>GROUP ›› https://t.me/HoiChoi_Group</b>"
+AJAX = "<b>𝙱𝙾𝚃 ›› <a href=https://t.me/{}>{}</a></b>"
 # -- Constants End -- #
 
 
@@ -29,9 +29,9 @@ async def ping(_, message):
     await rm.edit(f"Pong!\n{time_taken_s:.3f} ms")
 
 
-@Client.on_message(filters.command("repo", COMMAND_HAND_LER) & f_onw_fliter)
+@Client.on_message(filters.command("unmesh", COMMAND_HAND_LER) & f_onw_fliter)
 async def repo(_, message):
-    await message.reply_text(REPO)
+    await message.reply_text(UNMESH)
 
 
 @Client.on_message(filters.command("group", COMMAND_HAND_LER) & f_onw_fliter)
