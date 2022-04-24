2bef4feb211c47742ab33d702b62bfe32cb381e2
Aritra <88967619+Aritra-01@users.noreply.github.com>
2022-04-25 00:28:41 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Aadhi000/Ajax.git /Ajax
+  git clone https://github.com/Aritra-01/Ajax-Filter.git /Ajax
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Ajax
